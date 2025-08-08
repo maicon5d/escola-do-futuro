99d1bd1f25b8a2c0c3e51c49948d81f19d253fdf
maicon5d <maiconcarlose5@gmail.com>
2025-08-08 16:19:55 -0300

    08/08/2025
    Meu primeiro codigo conciente

---
+++ .py
@@ -0,0 +1,7 @@
+
+nome = input('qual o seu nome?')
+idade = input('qual a sua idade?')
+cidade = input('qual a sua cidade?')
+
+mensagem = f"Ola prazer {nome}, sua idade é {idade},reside em {cidade}"
+print(mensagem)
